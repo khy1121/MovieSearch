cdf4a490fcd5809ef74f89ed5ad5d04ea65a248a
hyKim <rlagjsdud3@hansung.ac.kr>
2025-07-08 00:13:07 +0900

    Add NotFound page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h2>404 - 페이지를 찾을 수 없습니다</h2>
+      <p>잘못된 경로로 접근하셨습니다.</p>
+    </div>
+  );
+}
+
+export default NotFound;
